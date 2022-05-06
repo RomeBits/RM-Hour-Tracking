4a40443de4724b6bdb1c59eb97d4f4af239135d6
Benjamin Rome <benjamin.rome10@gmail.com>
2022-05-06 11:20:16 -0400

    pandas and add csv for testing

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 # Written by Benjamin Rome 6-6-22
 # See README for more info on how to use
 import sys
+import pandas as pd
 
 if __name__ == "__main__":
-    CSV = sys.argv[1]
+    if len(sys.argv) != 2:  
+        print("ERROR: Usage: python -3 main.py < my_csv.csv >")
+        sys.exit(1)
+
+    CSV  = sys.argv[1]
+    data = pd.read_csv(CSV)
+
+    
+
+
+
+
 
