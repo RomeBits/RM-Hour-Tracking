e90523cdf18e5350b267f9f06e74e9c61455acc6
Benjamin Rome <benjamin.rome10@gmail.com>
2022-05-06 11:34:49 -0400

    main looop

--- main.py
+++ main.py
@@ -2,16 +2,29 @@
 # See README for more info on how to use
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    # Need to give us a CSV as input
     if len(sys.argv) != 2:  
         print("ERROR: Usage: python -3 main.py < my_csv.csv >")
         sys.exit(1)
 
+    # Collect CSV name and read it in as pandas datafram
     CSV  = sys.argv[1]
-    data = pd.read_csv(CSV)
+    data = pd.read_csv(CSV, names=['Timestamp', 'RCSID','Subsystem', 'In_Out'])
 
-    
+    # Main loop
+    #   Loop through the data find matching ins and outs for specific RCSID
+    for i in range(len(data['Timestamp'])):
+        if i == 0: continue
+        print(data['Timestamp'][i])
+        print(data['RCSID'][i])
+        print(data['Subsystem'][i])
+        print(data['In_Out'][i])
+
+
+
 
 
 
